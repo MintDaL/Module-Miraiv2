4394fbeaced56d37640e926b4c8846c448d24240
Nguyễn Minhh <76152941+MintDaL@users.noreply.github.com>
2022-02-13 11:48:43 +0700

    adminonly (có thể)

---
+++ modules/commands/refresh.js
@@ -0,0 +1,17 @@
+module.exports.config = {
+  name: "refresh",
+  version: "1.0.0",
+  hasPermssion: 1,
+  credits: "MintDaL",
+  description: "Làm mới dữ liệu thành viên nhóm",
+  commandCategory: "QTV-box",
+  cooldowns: 5,
+};
+
+module.exports.run = async function ({ api, event, Threads }) {
+  const { threadID, messageID } = event;
+  const threadInfo = await api.getThreadInfo(threadID);
+	await Threads.setData(threadID, { name: threadInfo.name, threadInfo });
+	global.data.threadInfo.set(threadID, threadInfo);
+  return api.sendMessage(`Done.`, threadID, messageID);
+}
