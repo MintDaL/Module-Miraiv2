3d371ed0573011b56c21d4687d314556e3aa0270
Nguyễn Minhh <76152941+MintDaL@users.noreply.github.com>
2022-02-28 08:22:20 +0700

    Update and rename adminonly.js to adminboxonly.js

--- modules/commands/adminboxonly.js
+++ modules/commands/adminboxonly.js
@@ -1,15 +1,14 @@
 /*
-* @author tho493
+* @author ProCoderMew
 * @warn Do not edit code or edit credits
-* fix bug by ProCoderMew
 * Để sử dụng lệnh hãy thay file listen.js vào
 */
 
 module.exports.config = {
-name: "adminonly",
+name: "adminboxonly",
 version: "1.0.1",
-hasPermssion: 2,
-credits: "tho493",
+hasPermssion: 1,
+credits: "ProCoderMew",
 description: "Cài đặt bot về chế độ chỉ admin bot được sử dụng bot",
 commandCategory: "system",
 cooldowns: 5,
@@ -26,5 +25,5 @@ else data["only_admin_group"] = false;
 
 await Threads.setData(threadID, { data });
 global.data.threadData.set(threadID, data);
-return api.sendMessage(`[ MODE ] » Đã ${(oa[threadID] == true) ? "bật" : "tắt"} chế độ chỉ admin được sử dụng bot.`, threadID, messageID);
+return api.sendMessage(`[ MODE ] » Đã ${(oa[threadID] == true) ? "bật" : "tắt"} chế độ chỉ quản trị viên được sử dụng bot.`, threadID, messageID);
 }
